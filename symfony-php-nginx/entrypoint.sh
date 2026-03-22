11517ec60de8cc00d1b8d78daa9b52658a00f6a6
sgj0 <contact@sgj0.net>
2026-03-22 01:37:37 +0100

    remove supervisor

--- symfony-php-nginx/entrypoint.sh
+++ symfony-php-nginx/entrypoint.sh
@@ -7,21 +7,28 @@ set -e
 # Idea taken from https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
 USER_ID=${LOCAL_USER_ID:-1000}
 
-if [ $USER_ID -eq 0 ]; then # for windows with WSL
-  echo "Starting as root"
+if [ ! -z $LOCAL_USER_ID ]; then
+  if [ $USER_ID -eq 0 ]; then # for windows with WSL
+    echo "Starting as root"
 
-  # Set php-fpm user to root
-  sed -i "s/user = www-data/user = root/g" /usr/local/etc/php-fpm.d/zz-www.conf
-  sed -i "s/group = www-data/user = root/g" /usr/local/etc/php-fpm.d/zz-www.conf
-else
-  echo "Starting with UID : $USER_ID"
+    # Set php-fpm user to root
+    sed -i "s/user = www-data/user = root/g" /usr/local/etc/php-fpm.d/zz-www.conf
+    sed -i "s/group = www-data/user = root/g" /usr/local/etc/php-fpm.d/zz-www.conf
+  else
+    echo "Starting with UID : $USER_ID"
 
-  # Fix git not happy with ownership on mac docker "fatal: detected dubious ownership in repository at '/app'"
-  # (similar to https://github.com/go-gitea/gitea/issues/19455)
-  # git config --system --add safe.directory /var/www/html
+    # Fix git not happy with ownership on mac docker "fatal: detected dubious ownership in repository at '/app'"
+    # (similar to https://github.com/go-gitea/gitea/issues/19455)
+    # git config --system --add safe.directory /var/www/html
 
-  usermod -u $USER_ID www-data
-  groupmod -g $USER_ID www-data
+    usermod -u $USER_ID www-data
+    groupmod -g $USER_ID www-data
+  fi
 fi
 
-exec "$@"
+php-fpm --allow-to-run-as-root &
+nginx -g "daemon off;" &
+
+wait -n
+
+exit $?
